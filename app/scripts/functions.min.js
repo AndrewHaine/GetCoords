3ac269814c07aa1da49ee17842db4566ec1fd0d5
AndrewHaine <andrewhaineweb@gmail.com>
2016-07-16 23:36:18 +0100

    Add some functionality to button and add callback to initialise google map

--- app/scripts/functions.min.js
+++ app/scripts/functions.min.js
@@ -14,3 +14,33 @@ $(document).ready(function() {
 });
 
 //Map Work
+
+
+$('#button').on('click', function(){
+  var places = new Array();
+
+  $('#results').empty();
+
+  for (var i = 0; i < 3; i++) {
+    var placeValue = document.getElementById('place_input_'+i).value;
+    if (placeValue != "") {
+      places.push(placeValue);
+    };
+  };
+  for (var place in places) {
+    $('#results').append('<div class="result-panel"><div class="placename">'+ places[place] + '</div><div class="longCo"><label for="LongCoIn">Longnitudinal:</label><input class="CoDisplay" value="52.439" readonly type="text" name="LongCoIn"></div><div class="latCo"><label for="LatCoIn">Latitudinal:</label><input class="CoDisplay" value="0.661" readonly type="text" name="LongCoIn"></div></div>');
+  }
+  console.log(places);
+});
+
+
+
+var map;
+function initMap() {
+  var myLatLng = {lat: -43.544, lng: 43.75};
+
+  map = new google.maps.Map(document.getElementById('map-screen'), {
+    center: myLatLng,
+    zoom: 5
+  });
+}
